ab6487e5c878312cad047363a6e88a4ca9faadda
Abdullahi Husain <abdulmohusain@gmail.com>
2024-03-21 15:26:53 +0100

    Mant to Many

--- models/amenity.py
+++ models/amenity.py
@@ -19,5 +19,9 @@ class Amenity(BaseModel, Base):
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    # place_amenities = relationship("Place", secondary="place_amenity",
-    #                                viewonly=False)
+    place_amenities = relationship(
+        "Place",
+        secondary="place_amenity",
+        viewonly=False,
+        back_populates='amenities'
+    )

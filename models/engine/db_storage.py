60b2df840bf60562bda5fc4a92816e71d84a4b87
Abdullahi Husain <abdulmohusain@gmail.com>
2024-03-21 02:03:20 +0100

    DBStorage - States and Cities

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -48,7 +48,7 @@ class DBStorage:
         if cls is None:
             objs = self.__session.query(State).all()
             objs.extend(self.__session.query(City).all())
-            # objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(User).all())
             # objs.extend(self.__session.query(Place).all())
             # objs.extend(self.__session.query(Review).all())
             # objs.extend(self.__session.query(Amenity).all())

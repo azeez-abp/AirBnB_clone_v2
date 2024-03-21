eb3d416695b5b086cd765a8696756f3e98ff9faa
Abdullahi Husain <abdulmohusain@gmail.com>
2024-03-21 02:07:09 +0100

    DBStorage - States and Cities

--- models/user.py
+++ models/user.py
@@ -19,13 +19,3 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
-        # places = relationship(
-        #     'Place',
-        #     back_populates='user',
-        #     cascade='all, delete-orphan'
-        # )
-        # reviews = relationship(
-        #     'Review',
-        #     backref='user',
-        #     cascade='all, delete, delete-orphan'
-        # )

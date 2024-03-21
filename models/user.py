2c8c48b83054ee0fcd2a458636fd018ebe7a98ef
Abdullahi Husain <abdulmohusain@gmail.com>
2024-03-21 02:00:52 +0100

    DBStorage - States and Cities

--- models/user.py
+++ models/user.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from os import getenv
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     if getenv("HBNB_TYPE_STORAGE") != "db":
